e8ef0614ffb7e8c2816a3207bfb5cf9ca0228fc9
yumechi <yumechi0525@gmail.com>
2020-02-23 22:42:39 +0900

    fix index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,52 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank"> documentation </a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">
-            Nuxt Documentation
-          </a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+  <v-layout>
+    <v-flex>
+      <h1>LT用ツールを集めたサイト</h1>
+      <p>
+        LT用のツールを集めたサイトです。どうぞよろしく。
+      </p>
+
+      <v-container>
+        <v-row dense>
+          <v-col cols="12">
+            <v-card class="mx-auto">
+              <v-card-text>
+                <p class="display-1 text--primary">
+                  発表順を決める
+                </p>
+                <div class="text--primary">
+                  まずは発表順を決めましょう。
+                </div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn text color="deep-purple accent-4">
+                  <a href="./shuffle"> こちらから </a>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+
+            <v-card class="mx-auto">
+              <v-card-text>
+                <p class="display-1 text--primary">
+                  時間を測る
+                </p>
+                <div class="text--primary">
+                  時間の計測をしましょう（この機能は実装中です）
+                </div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn text color="deep-purple accent-4">
+                  <a href="./timer"> こちらから </a>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
-}
+export default {}
 </script>
